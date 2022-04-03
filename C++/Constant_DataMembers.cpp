f5f00bf6d4d5043135c162c18eaa42f32e7a04b5
Akash-Pinate <78008539+Akash-Pinate@users.noreply.github.com>
2022-04-03 11:37:19 +0530

    Constant data members in c++ by using class

---
+++ C++/Constant_DataMembers.cpp
@@ -0,0 +1,53 @@
+/*
+	THIS PROGRAM ILLUSTRATES THE USE OF : 
+		DEFINING 'constant data-member' in the class...
+*/
+#include<iostream>
+
+using namespace std;
+
+class demo
+{
+	public:
+		const int i;
+	/*
+		// 	Constant members must be initialised
+	        //      otherwise compiler generate error.
+                //      but whenever we create the the object of the class theire no memmory allocation 
+		demo()
+		{
+		
+		}
+	*/
+
+	/*
+		// 	We can not initialise constant members 
+		// 		inside the constuctor
+		demo()
+		{
+			i = 10;	//Error
+		}
+	*/
+
+		// 	CORRECT WAY TO DEFINE constant members...
+    	        // 		To initialise the constant members we have to use the member initialisation list concept
+                //    To initialize the constant members before control goes inside the the constructor we have to intialize the constant members 
+
+		demo():i(10)		// THIS IS concept of 'initialisation list'...
+		{
+			cout<<"Successfully initialised the constant members..."<<endl;
+		}
+};
+
+int main()
+{
+	demo dobj;
+
+	// We can not change value of constant member...
+	// dobj.i++;		// NOT ALLOWED...
+
+	return 0;
+}
+
+
+//constant members is also known as READ-ONLY variable/member 
