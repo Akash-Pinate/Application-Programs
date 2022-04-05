8322c0bbe4a37d74ae99820145c94dc7b09bef13
Akash-Pinate <78008539+Akash-Pinate@users.noreply.github.com>
2022-04-06 01:08:15 +0530

    Defination, Oveloaded Defination and Re-Defination

---
+++ C++/Definations.cpp
@@ -0,0 +1,61 @@
+/* THE FOLLOWING PROGRAM WILL ILLUSTRATES :
+            DEFINATION ,OVERLOADED DEFINATION AND  RE-DEFINATION 
+*/
+
+/*
+      DEFINATION-> When we define any method in a class for first time 
+      
+      
+      OVERLOADED DEFINATIONS-> If we define more than one function in same name and
+                               different prototype
+                               
+
+      RE-DEFINATION -> if a base class contains one method and if derived class contain 
+                       same method with same and different prototype
+
+*/
+
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i,j;
+        
+        //Defination
+        void fun()
+        {
+            cout<<"inside fun: "<<endl;
+        }
+
+        void gun()
+        {
+            cout<<"inside gun: "<<endl;
+        }
+        
+        //Overloaded Defination
+        void fun(int a)
+        {
+            i=a;
+            cout<<"parameterised fun: "<<endl;
+        }
+}
+
+class Hello: public Demo
+{
+    public:
+        int x,y;
+         
+        //Re-Defination
+        void fun(int a)
+        {
+            cout<<"inside hello"<<endl;
+        }
+}
+
+int main()
+{
+  
+  return 0;
+}
